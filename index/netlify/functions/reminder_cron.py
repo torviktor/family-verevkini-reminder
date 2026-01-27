f76b04b41be50b998dd52e78cf7ffc0ab9cc6442
torviktor <ckkbiouo@gmail.com>
2026-01-27 21:04:02 +0300

    Update reminder_cron.py

--- index/netlify/functions/reminder_cron.py
+++ index/netlify/functions/reminder_cron.py
@@ -1,28 +1,31 @@
 import json
 from datetime import datetime, timedelta
+import asyncio
 from telegram import Bot
 
-TOKEN = "ВСТАВЬ_СЮДА_TOKEN_ОТ_BOTFATHER"
-DATA_FILE = "data/events.json"
+TOKEN = "8593589562:AAEzpabdygda057aFSyDrUq3mIwkWoKsKVY"
+DATA_FILE = "/tmp/events.json"
 
-
-def handler(event, context):
+async def send_reminders():
     bot = Bot(TOKEN)
     now = datetime.utcnow()
-
-    with open(DATA_FILE, "r", encoding="utf-8") as f:
-        data = json.load(f)
-
+    
+    try:
+        with open(DATA_FILE, "r", encoding="utf-8") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        return
+    
     for e in data["events"]:
         event_time = datetime.fromisoformat(e["event_time"])
-
         for m in e["notify_minutes"]:
             notify_time = event_time - timedelta(minutes=m)
-
             if abs((notify_time - now).total_seconds()) < 150:
-                bot.send_message(
+                await bot.send_message(
                     e["chat_id"],
                     f"⏰ Напоминание:\n{e['title']}\nчерез {m} мин."
                 )
 
+def handler(event, context):
+    asyncio.run(send_reminders())
     return {"statusCode": 200}
